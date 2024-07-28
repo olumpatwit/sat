810e4540082784d2a1416f3b9f7dee97717b2d9c
Perry Olum <olump@wit.edu>
2024-07-28 00:55:22 -0400

    Start thinking about the second part with some notes on switchworld
    I think I'm going to write out an extension of Assignment 5 to generate
    a SAT problem as the first phase of part 2. And then time will tell if I
    end up trying any other ideas.

--- CDCL.py
+++ CDCL.py
@@ -102,26 +102,22 @@ def cdcl(clauses):
     rv = {k:v.value for k,v in assignments.items()}
     assert satisfies_list(clauses, rv)
     return rv
-"""
 clauses = [{n(2),n(3),n(4),e(5)},{n(1),n(5),e(6)},{n(5),e(7)},
            {n(1),n(6),n(7)},{n(1),n(2),e(5)},{n(1),n(3),e(5)},
            {n(1),n(4),e(5)},{n(1),e(2),e(3),e(4),e(5),n(6)}]
 
-random.seed(1)
 for x in range(10,500,10):
     clauses = generate_random_clause(3,x*4,x)
     import timeit
     print("---",x)
     print("cdcl", timeit.timeit("print(cdcl(clauses))","from __main__ import cdcl,clauses",number=1))
     print("cdcl" ,backtracks)
-    if x < 100:
+    if x < 125:
         print("dpll", timeit.timeit("DPLL2(clauses)","from __main__ import DPLL2,clauses",number=1))
         print("dpll", SAT.falsechecks)
     SAT.falsechecks = backtracks = 0
-"""
-random.seed(11)
 cdcl([[n(1),n(0)],[e(1),e(0)]])
 print("Welcome to the new world")
-nn=220
-g = generate_random_clause(3,int(4.2*nn),nn)
+nn=230
+g = generate_random_clause(3,int(4*nn),nn)
 print(cdcl(g))
